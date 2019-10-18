e283b302a8df30f9844a4a0aea16d024a98f3236
razorloves <razorloves@gmail.com>
2019-10-18 00:06:22 -0500

    b4s4: Move eUICC permissions xml to /product
    All eUICC items were moved to /product in Q.
    
    Also, move it to lineage specific makefile.
    
    Change-Id: I614f86b16f61959c47465863ab1d5d55030209ab

--- device-lineage.mk
+++ device-lineage.mk
@@ -14,17 +14,34 @@ PRODUCT_PROPERTY_OVERRIDES += \
 DEVICE_PACKAGE_OVERLAYS += device/google/bonito/overlay-lineage
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += device/google/bonito/overlay-lineage/lineage-sdk
 
+
 # EUICC
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:system/etc/permissions/android.hardware.telephony.euicc.xml
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+
+# GMS
+WITH_GMS_FI := true
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
 
+
 # LMK
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.lmk.use_psi=true
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/Bluetooth
+
+# Partitions
+AB_OTA_PARTITIONS += \
+    vendor
+
+# Privapp Permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/permissions/privapp-permissions-aosp-extended.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-aosp-extended.xml
 
 # Preopt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += \
@@ -53,6 +70,3 @@ PRODUCT_PACKAGES += \
     libwifi-hal-qcom
 
 WITH_GMS_FI := true
-
-PRODUCT_COPY_FILES += \
-    device/google/bonito/permissions/privapp-permissions-aosp-extended.xml:system/etc/permissions/privapp-permissions-aosp-extended.xml
