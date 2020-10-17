7e4b63071d8ebe8909e4cfd820154fc81651a377
abhizala <abhizala@hotmail.com>
2020-10-17 12:35:23 +0530

    b4s4: change heap directory

--- device.mk
+++ device.mk
@@ -646,7 +646,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 endif
 
 # Setup Dalvik VM configurations
-$(call inherit-product, vendor/extras/configs/phone-xhdpi-4096-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 PRODUCT_COPY_FILES += \
     device/google/bonito/fstab.hardware:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.$(PRODUCT_PLATFORM) \
