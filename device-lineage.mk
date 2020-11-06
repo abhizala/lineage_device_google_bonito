2df429f3777d2b1ee5cfc0914ed4dc1c5e4a236c
Josh Chasky <jchasky@gmail.com>
2020-11-06 13:03:26 +0530

    Expand AOSP compiled packages list for vendor

--- device-lineage.mk
+++ device-lineage.mk
@@ -52,3 +52,45 @@ WITH_GMS_FI := true
 
 PRODUCT_COPY_FILES += \
     device/google/bonito/permissions/privapp-permissions-aosp-extended.xml:system/etc/permissions/privapp-permissions-aosp-extended.xml
+
+# Build necessary packages for system
+PRODUCT_PACKAGES += \
+    libmediaplayerservice \
+    libstagefright_httplive:64
+
+# Missing vendor packages
+PRODUCT_PACKAGES += \
+    chre \
+    ese_spi_nxp:64 \
+    hardware.google.light@1.0.vendor \
+    libavservices_minijail_vendor:32 \
+    libcodec2_hidl@1.0.vendor:32 \
+    libcodec2_vndk.vendor \
+    libcppbor.vendor:64 \
+    libdrm.vendor \
+    libhidltransport.vendor \
+    libhwbinder.vendor \
+    libkeymaster_messages.vendor:64 \
+    libkeymaster_portable.vendor:64 \
+    libnetfilter_conntrack:64 \
+    libnfnetlink:64 \
+    libnos:64 \
+    libnos_client_citadel:64 \
+    libnos_datagram:64 \
+    libnos_datagram_citadel:64 \
+    libnosprotos:64 \
+    libnos_transport:64 \
+    libpuresoftkeymasterdevice.vendor:64 \
+    libsensorndkbridge:64 \
+    libsoft_attestation_cert.vendor:64 \
+    libteeui_hal_support.vendor:64 \
+    libtinycompress \
+    libwifi-hal:64 \
+    nos_app_avb:64 \
+    nos_app_identity:64 \
+    nos_app_keymaster:64 \
+    nos_app_weaver:64 \
+    vendor.display.config@1.0.vendor \
+    vendor.display.config@1.1.vendor \
+    vendor.display.config@1.2.vendor \
+    vendor.display.config@1.3.vendor
