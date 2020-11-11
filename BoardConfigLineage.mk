LineageOS/android_device_google_bonito

b4s4: Compile kernel in-line
* Define need for DTBO image and remove prebuilt logic.
* No need for prebuilt kernel/kernel modules logic as we'll never use it.
* We use Clang built kernel.

Change-Id: I9483a46c294cbc1edf594cf5ec77556d36e20735

BoardConfigLineage.mk
@@ -1,3 +1,11 @@
 # Common board config for crosshatch, blueline
 
+BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CONFIG := lineageos_crosshatch_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_SOURCE := kernel/google/crosshatch
+
 -include vendor/google/crosshatch/BoardConfigVendor.mk

abhizala/lineage_device_google_bonito

BoardConfigLineage.mk
@@ -1,3 +1,11 @@
 # Common board config for bonito, sargo
 
+BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CONFIG := lineageos_bonito_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_SOURCE := kernel/google/bonito
+
 -include vendor/google/bonito/BoardConfigVendor.mk