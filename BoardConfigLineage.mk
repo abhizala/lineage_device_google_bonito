70b9ec4622978960ef20a2785d70663e10d32c32
abhizala <abhizala@hotmail.com>
2020-11-11 14:48:48 +0530

    b4s4: let's build gulch

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -13,7 +13,7 @@ TARGET_KERNEL_ADDITIONAL_FLAGS := \
     MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CONFIG := lineageos_bonito_defconfig
+TARGET_KERNEL_CONFIG := gulch_inline_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/bonito
 
 BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
