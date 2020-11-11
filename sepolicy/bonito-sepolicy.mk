LineageOS/android_device_google_bonito

bonito: Update sepolicy path after subtree-ing
Change-Id: I06341995fac6f27c05192f9f1fc0d61d190b82f5

sepolicy/bonito-sepolicy.mk
@@ -1,8 +1,8 @@
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR := device/google/bonito-sepolicy/public
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR := device/google/bonito-sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR := device/google/bonito/sepolicy/public
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR := device/google/bonito/sepolicy/private
 
 # vendors
-BOARD_SEPOLICY_DIRS += device/google/bonito-sepolicy/vendor/qcom/common
-BOARD_SEPOLICY_DIRS += device/google/bonito-sepolicy/vendor/qcom/sdm710
-BOARD_SEPOLICY_DIRS += device/google/bonito-sepolicy/vendor/google
-BOARD_SEPOLICY_DIRS += device/google/bonito-sepolicy/vendor/verizon
+BOARD_SEPOLICY_DIRS += device/google/bonito/sepolicy/vendor/qcom/common
+BOARD_SEPOLICY_DIRS += device/google/bonito/sepolicy/vendor/qcom/sdm710
+BOARD_SEPOLICY_DIRS += device/google/bonito/sepolicy/vendor/google
+BOARD_SEPOLICY_DIRS += device/google/bonito/sepolicy/vendor/verizon

abhizala/lineage_device_google_bonito

sepolicy/bonito-sepolicy.mk
@@ -1,9 +1,12 @@
-PRODUCT_PUBLIC_SEPOLICY_DIRS += device/google/bonito-sepolicy/public
-PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/bonito-sepolicy/private
+PRODUCT_PUBLIC_SEPOLICY_DIRS += device/google/bonito/sepolicy/public
+PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/bonito/sepolicy/private
 
 # vendors
-BOARD_VENDOR_SEPOLICY_DIRS += device/google/bonito-sepolicy/vendor/qcom/common
-BOARD_VENDOR_SEPOLICY_DIRS += device/google/bonito-sepolicy/vendor/qcom/sdm710
-BOARD_VENDOR_SEPOLICY_DIRS += device/google/bonito-sepolicy/vendor/google
-BOARD_VENDOR_SEPOLICY_DIRS += device/google/bonito-sepolicy/vendor/verizon
-BOARD_VENDOR_SEPOLICY_DIRS += device/google/bonito-sepolicy/tracking_denials
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/bonito/sepolicy/vendor/qcom/common
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/bonito/sepolicy/vendor/qcom/sdm710
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/bonito/sepolicy/vendor/google
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/bonito/sepolicy/vendor/verizon
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/bonito/sepolicy/tracking_denials
+
+
+